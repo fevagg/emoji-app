c3d68e0200e1208b3b1cb142c7ec71fa97a7b00b
fevagg <paingrille19@gmail.com>
2022-09-22 12:20:59 -0300

    Fixing route errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Layout from "./components/Layout";
@@ -11,33 +16,23 @@ import AllEmojis from "./pages/Emojis/AllEmojis";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ErrorPage from "./pages/Error/Error";
+import Loading from "./components/Loading";
 
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    path: "/found-ji",
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        element: <Home />,
-        path: "",
-      },
-      { element: <About />, path: "about" },
-      {
-        element: <AllEmojis />,
-        path: "emojis",
-      },
-      {
-        path: "emojis/:emojiUnicode",
-        element: <SelectedEmoji />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/emojis" element={<AllEmojis />} />
+      <Route path="/emojis/:emojiUnicode" element={<SelectedEmoji />} />
+    </Route>
+  ),
+  { basename: "/found-ji" }
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} fallbackElement={<Loading />} />
   </React.StrictMode>
 );
